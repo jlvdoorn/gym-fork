c11701d63cfe811c75184b1353e16ba96e4f3207
Jan van Doorn <jlvdoorn@outlook.com>
2022-06-17 14:58:48 +0200

    updated printing

--- main.py
+++ main.py
@@ -2,101 +2,110 @@ import gym
 import time
 import numpy as np
 
-env = gym.make(
-    "LunarLander-v2",
-    continuous = True,
-    gravity = -9.81,
-    enable_wind = False,
-    wind_power = 7.5,
-    turbulence_power = 0.5,
-)
-observation, info = env.reset(seed=42, return_info=True)
+class Sim:
 
-def iterate(steps=1000):
+    def __init__(self, steps: int =1000):
+        self.steps = steps
+        self.env = gym.make(
+            "LunarLander-v2",
+            continuous = False,
+            gravity = -9.81,
+            enable_wind = False,
+            wind_power = 7.5,
+            turbulence_power = 0.5,
+        )
+        observation, info = self.env.reset(seed=42, return_info=True)
 
-    simData = {}
+    def iterate(self):
 
-    act = {}
-    obs = {}
-    rwd = {}
-    don = {}
-    inf = {}
+        simData = {}
 
-    episode = 1
+        act = {}
+        obs = {}
+        rwd = {}
+        don = {}
+        inf = {}
 
-    for k in range(steps):
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        env.render(mode='human')
-        print('--------------------------------')
-        print('#: '+str(k))
-        print('a: '+str(action))
-        print('t: '+str(observation[4]*180/np.pi))
-        print('x: '+str(observation[0]))
-        print('y: '+str(observation[1]))
-        if info != {}:
-            print('i: '+str(info))
+        episode = 1
 
-        act[k] = action
-        obs[k] = observation
-        rwd[k] = reward
-        don[k] = done
-        inf[k] = info
+        for k in range(self.steps):
+            action = self.env.action_space.sample()
+            observation, reward, done, info = self.env.step(action)
+            self.env.render(mode='human')
+            # print('--------------------------------')
+            # print('#: '+str(k))
+            # print('a: '+str(action))
+            # print('t: '+str(observation[4]*180/np.pi))
+            # print('x: '+str(observation[0]))
+            # print('y: '+str(observation[1]))
+            # if info != {}:
+            #     print('i: '+str(info))
 
+            act[k] = action
+            obs[k] = observation
+            rwd[k] = reward
+            don[k] = done
+            inf[k] = info
+
+            if done:
+                ep = {}
+                ep['act'] = act
+                ep['obs'] = obs
+                ep['rwd'] = rwd
+                ep['don'] = don
+                ep['inf'] = inf
+
+                simData[episode] = ep
+                
+                episode = episode + 1
+
+                act = {}
+                obs = {}
+                rwd = {}
+                don = {}
+                inf = {}
+
+                observation, info = self.env.reset(return_info=True)
+
+            if k == self.steps-1: # finish current episode
+                ep = {}
+                ep['act'] = act
+                ep['obs'] = obs
+                ep['rwd'] = rwd
+                ep['don'] = don
+                ep['inf'] = inf
+
+                simData[episode] = ep
+
+        self.simData = simData
+
+    def oneStep(self):
+
+        action = self.env.action_space.sample()
+        observation, reward, done, info = self.env.step(action)
+        self.env.render(mode='human')
+        
         if done:
-            ep = {}
-            ep['act'] = act
-            ep['obs'] = obs
-            ep['rwd'] = rwd
-            ep['don'] = don
-            ep['inf'] = inf
+            observation, info = self.env.reset(return_info=True)
 
-            simData[episode] = ep
-            
-            episode = episode + 1
+    def printSimStats(self):
+        print('###################')
+        print('Simulation done')
+        print('# Episodes: '+str(len(self.simData)))
+        print('# Iterations: '+str(self.steps))
+        print('###################')
 
-            act = {}
-            obs = {}
-            rwd = {}
-            don = {}
-            inf = {}
-
-            observation, info = env.reset(return_info=True)
-
-        if k == steps-1: # finish current episode
-            ep = {}
-            ep['act'] = act
-            ep['obs'] = obs
-            ep['rwd'] = rwd
-            ep['don'] = don
-            ep['inf'] = inf
-
-            simData[episode] = ep
-
-    return simData
-
-def oneStep():
-
-    action = env.action_space.sample()
-    observation, reward, done, info = env.step(action)
-    env.render(mode='human')
-    
-    if done:
-        observation, info = env.reset(return_info=True)
-
-def printSimStats(simData):
-    print('Simulation done')
-    print('# Episodes: '+str(len(simData)))
-
-    for k in range(1,len(simData)+1):
-        print('Episode '+str(k))
-        print(str(len(simData[k]['act']))+' iterations')
-        print(str(min(simData[k]['rwd']))+' min reward')
-        print(str(max(simData[k]['rwd']))+' max reward')
+        for k in range(1,len(self.simData)+1):
+            print('Episode '+str(k))
+            print('Iterations: '+str(len(self.simData[k]['act'])))
+            print('Min reward: '+str(min(self.simData[k]['rwd'].values())))
+            print('Max reward: '+str(max(self.simData[k]['rwd'].values())))
+            print('###################')
 
 
 if __name__ == "__main__":
-    simData = iterate(500)
-    printSimStats(simData)
+    falcon9 = Sim(10)
+    falcon9.iterate()
+    falcon9.printSimStats()
     print('done')
     #env.close()
