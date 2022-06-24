ae401d8e7abb67166e6857be3ff7bee759c99386
Jan van Doorn <jlvdoorn@outlook.com>
2022-06-24 11:50:45 +0200

    committed to commit

--- main.py
+++ main.py
@@ -208,7 +208,7 @@ class DQN:
 
 if __name__ == "__main__":
     # 400 episodes of max 7000 steps
-    falcon9 = Sim(7000, 'human', 7000)
+    falcon9 = Sim(7000, None, 7000)
     falcon9.trainDQN(200)
     falcon9.plotGraph()
     falcon9.writeOutput()
